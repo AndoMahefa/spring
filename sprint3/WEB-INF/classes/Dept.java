44d150e9c9521f695035b8ba479cb2f97648821f
AndoMahefa <razafimahatratramahefa7@gmail.com>
2023-03-31 09:41:56 +0300

    sprint3

---
+++ sprint3/WEB-INF/classes/Dept.java
@@ -0,0 +1,14 @@
+package models;
+
+import etu1896.annotation.*;
+
+public class Dept {
+    @Url(url = "all-dept")
+    public void find_all_dept() {
+
+    }
+
+    public void insert() {
+
+    }
+}
---
+++ sprint3/WEB-INF/classes/Emp.java
@@ -0,0 +1,20 @@
+package models;
+
+import etu1896.annotation.*;
+
+public class Emp {
+    
+    @Url(url = "all-emp")
+    public void find_all_emp(){
+
+    }
+
+    @Url(url = "insert-emp")
+    public void insert_emp(){
+
+    }
+
+    public void test(){
+        
+    }
+}
