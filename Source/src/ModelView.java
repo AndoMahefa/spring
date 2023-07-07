990eb229820fe71932cb86e0bd5909258d72e731
AndoMahefa <razafimahatratramahefa7@gmail.com>
2023-07-07 22:36:51 +0300

    sprint10

--- Source/src/ModelView.java
+++ Source/src/ModelView.java
@@ -3,12 +3,20 @@ package etu1896.framework;
 import java.lang.reflect.*;
 import java.util.*;
 import java.sql.Date;
-
 import etu1896.framework.Utilitaire;
 
 public class ModelView {
   String view;
-  HashMap<String,Object> data = new HashMap<String, Object>();
+  HashMap<String, Object> data = new HashMap<String, Object>();
+  HashMap<String, Object> sessions = new HashMap<String, Object>();
+
+  public HashMap<String, Object> getSessions() {
+    return sessions;
+  }
+
+  public void setSessions(HashMap<String, Object> sessions) {
+    this.sessions = sessions;
+  }
 
   public HashMap<String, Object> getData() {
     return data;
@@ -35,36 +43,6 @@ public class ModelView {
 
   }
 
-  // public static ModelView loadView(String url, HashMap<String, Mapping> mappingUrls, String id) throws Exception {
-  //   Set<String> set = mappingUrls.keySet();
-  //   if (!set.contains(url)) {
-  //     throw new Exception("404 not found!");
-  //   }
-
-  //   String className = mappingUrls.get(url).getClassName();
-  //   String methodName = mappingUrls.get(url).getMethod();
-  //   System.out.println(className);
-  //   System.out.println(methodName);
-  //   Class<?> classe = Class.forName(className);
-  //   Constructor<?> constructor = classe.getDeclaredConstructor();
-  //   Object object = constructor.newInstance();
-  //   Method method = null;
-  //   ModelView view = null;
-  //   if(id != null) {
-  //     // System.out.println("mety?");
-  //     method = classe.getDeclaredMethod(methodName, Integer.class);
-  //     // System.out.println(method);
-  //     view = (ModelView) method.invoke(object, Integer.valueOf(id));
-  //     // System.out.println(view);
-  //     // System.out.println(view.getView());
-  //   } else { 
-  //     method = classe.getDeclaredMethod(methodName);
-  //     view = (ModelView) method.invoke(object);
-  //   }
-    
-  //   return view;
-  // }
-
   public static ModelView loadView(String url, HashMap<String, Mapping> mappingUrls, Vector<String> values) throws Exception {
     Set<String> set = mappingUrls.keySet();
     if (!set.contains(url)) {
@@ -73,23 +51,19 @@ public class ModelView {
 
     String className = mappingUrls.get(url).getClassName();
     String methodName = mappingUrls.get(url).getMethod();
-    // System.out.println(className);
-    // System.out.println(methodName);
-    // System.out.println("tonga ato ve?");
     Class<?> classe = Class.forName(className);
     Constructor<?> constructor = classe.getDeclaredConstructor();
     Object object = constructor.newInstance();
     Method method = null;
     ModelView view = null;
-    // System.out.println(values.size());
     Object[] vals = new Object[values.size()];
     if(values.size() != 0) {
-      System.out.println(values.size() + " 222");
       method = new Utilitaire().getMethod(object, methodName);
       Class<?>[] paramsType = new Utilitaire().getParameters(method);
       System.out.println("taille: "+paramsType.length);
       
       for (int i = 0; i < paramsType.length; i++) {
+        System.out.println(paramsType[i].getSimpleName());
         if(paramsType[i].getSimpleName().equalsIgnoreCase("integer") ) {
           int val = Integer.parseInt(values.get(i));
           vals[i] = val;
@@ -122,4 +96,7 @@ public class ModelView {
     this.getData().put(key,value);
   }
  
+  public void addSessions(String key, Object value) {
+    this.getSessions().put(key, value);   
+  }
 }
